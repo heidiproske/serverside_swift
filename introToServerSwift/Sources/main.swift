17b4db59d7b1f976e43f0c9633faa8758b38b6d7
Proske, Heidi <Heidi.Proske@capitalone.com>
2017-03-04 20:44:52 -0800

    Intro - Configure multiple routers so each can do a small task and then call next in the pipeline
    Result in web:
      Hello, KituraHere's the second completion

--- introToServerSwift/Sources/main.swift
+++ introToServerSwift/Sources/main.swift
@@ -7,6 +7,17 @@ Log.info("About to initialize our router")
 
 let router = Router()
 
+// Don't care if it was a GET, POST etc - hence using all
+router.all("/") { (request, response, next) in
+    response.send("Hello, Kitura")
+    next() // like a pipeline
+}
+
+router.all("/") { (request, response, next) in
+    response.send("Here's the second completion")
+    next()
+}
+
 Kitura.addHTTPServer(onPort: 8090, with: router) // Any port above 1024 is available for any user, under it requires admin
 
 Kitura.run() // Any code after this line will never run
