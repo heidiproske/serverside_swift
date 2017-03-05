8a916174b40c83be61a231d5da15785f6aa7d2e4
Proske, Heidi <Heidi.Proske@capitalone.com>
2017-03-04 20:41:24 -0800

    Intro - Start Kitura Server
    Press Cmd+R in Xcode and once it says “Running server” then in a browser, go to:
      http://localhost:8090
    and you’ll see:
      “Your Kitura based server is up and running!”

--- introToServerSwift/Sources/main.swift
+++ introToServerSwift/Sources/main.swift
@@ -1,1 +1,7 @@
-print("Hello, Kitura!")
+import Kitura
+
+let router = Router()
+
+Kitura.addHTTPServer(onPort: 8090, with: router) // Any port above 1024 is available for any user, under it requires admin
+
+Kitura.run() // Any code after this line will never run
